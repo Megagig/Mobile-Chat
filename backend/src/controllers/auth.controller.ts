f85682bd991c19fec0a923319b0ed861ab1685db
Megagig <megagigsoftwaresolutions.com>
2025-02-14 18:12:45 +0100

    Completed the Signup controller logic

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,7 +1,67 @@
-import { Request, Response } from 'express';
+import e, { Request, Response } from 'express';
+import prisma from '../db/prisma.js';
+import bcryptjs from 'bcryptjs';
+import generateToken from '../utils/generateToken.js';
+
+export const signup = async (req: Request, res: Response) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+
+    if (!fullName || !username || !password || !confirmPassword || !gender) {
+      return res.status(400).json({ error: 'All fields are required' });
+    }
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: 'Passwords do not match' });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
+
+    if (user) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
+    //Hash password
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    // https://avatar-placeholder.iran.liara.run/
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = await prisma.user.create({
+      data: {
+        fullName,
+        username,
+        password: hashedPassword,
+        gender,
+        profilePic: gender === 'male' ? boyProfilePic : girlProfilePic,
+      },
+    });
+
+    if (newUser) {
+      //generate token
+      generateToken(newUser.id, res);
+      res.status(201).json({
+        message: 'User created successfully',
+        user: {
+          id: newUser.id,
+          fullName: newUser.fullName,
+          username: newUser.username,
+          boyProfilePic: newUser.profilePic,
+        },
+      });
+    } else {
+      res.status(400).json({ error: 'User not created, invalid user data' });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
 
 export const login = async (req: Request, res: Response) => {};
 
 export const logout = async (req: Request, res: Response) => {};
-
-export const signup = async (req: Request, res: Response) => {};
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,12 +7,10 @@ dotenv.config();
 
 const app = express();
 
+app.use(express.json()); // for parsing application/json
+
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/messages', messageRoutes);
-
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
 
 app.listen(5000, () => {
   console.log('app listening on port 5000!');
