574215e8d81e80751659dbf124d1038593b6cbea
Megagig <megagigsoftwaresolutions.com>
2025-02-16 23:34:56 +0100

    Implement Signup functionality

--- frontend/src/components/GenderCheckbox.tsx
+++ frontend/src/components/GenderCheckbox.tsx
@@ -1,10 +1,21 @@
-const GenderCheckbox = () => {
+const GenderCheckbox = ({
+  selectedGender,
+  onCheckboxChange,
+}: {
+  selectedGender: string;
+  onCheckboxChange: (gender: 'male' | 'female') => void;
+}) => {
   return (
     <div className="flex">
       <div className="form-control">
         <label className={`label gap-2 cursor-pointer`}>
           <span className="label-text text-white">Male</span>
-          <input type="checkbox" className="checkbox border-slate-900" />
+          <input
+            type="checkbox"
+            className="checkbox border-slate-900"
+            checked={selectedGender === 'male'}
+            onChange={() => onCheckboxChange('male')}
+          />
         </label>
       </div>
       <div className="form-control">
@@ -13,6 +24,8 @@ const GenderCheckbox = () => {
           <input
             type="checkbox"
             className="checkbox border-slate-900 text-white"
+            checked={selectedGender === 'female'}
+            onChange={() => onCheckboxChange('female')}
           />
         </label>
       </div>
--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,7 +1,28 @@
 import { Link } from 'react-router-dom';
 import GenderCheckbox from '../components/GenderCheckbox';
+import { useState } from 'react';
+import useSignup from '../hooks/useSIgnup';
 
 const SignUp = () => {
+  const [inputs, setInputs] = useState({
+    fullName: '',
+    username: '',
+    password: '',
+    confirmPassword: '',
+    gender: '',
+  });
+
+  const { loading, signup } = useSignup();
+
+  const handleCheckboxChange = (gender: 'male' | 'female') => {
+    setInputs({ ...inputs, gender });
+  };
+
+  const handleSubmitForm = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await signup(inputs);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
       <div
@@ -14,7 +35,7 @@ const SignUp = () => {
           Sign Up
         </h1>
 
-        <form>
+        <form onSubmit={handleSubmitForm}>
           <div>
             <label className="label p-2">
               <span className="text-base label-text text-white">Full Name</span>
@@ -23,6 +44,10 @@ const SignUp = () => {
               type="text"
               placeholder="John Doe"
               className="w-full input input-bordered  h-10"
+              value={inputs.fullName}
+              onChange={(e) =>
+                setInputs({ ...inputs, fullName: e.target.value })
+              }
             />
           </div>
 
@@ -34,6 +59,10 @@ const SignUp = () => {
               type="text"
               placeholder="johndoe"
               className="w-full input input-bordered h-10"
+              value={inputs.username}
+              onChange={(e) =>
+                setInputs({ ...inputs, username: e.target.value })
+              }
             />
           </div>
 
@@ -45,6 +74,10 @@ const SignUp = () => {
               type="password"
               placeholder="Enter Password"
               className="w-full input input-bordered h-10"
+              value={inputs.password}
+              onChange={(e) =>
+                setInputs({ ...inputs, password: e.target.value })
+              }
             />
           </div>
 
@@ -58,10 +91,17 @@ const SignUp = () => {
               type="password"
               placeholder="Confirm Password"
               className="w-full input input-bordered h-10"
+              value={inputs.confirmPassword}
+              onChange={(e) =>
+                setInputs({ ...inputs, confirmPassword: e.target.value })
+              }
             />
           </div>
 
-          <GenderCheckbox />
+          <GenderCheckbox
+            selectedGender={inputs.gender}
+            onCheckboxChange={handleCheckboxChange}
+          />
 
           <Link
             to={'/login'}
@@ -71,8 +111,11 @@ const SignUp = () => {
           </Link>
 
           <div>
-            <button className="btn btn-block btn-sm mt-2 border border-slate-700">
-              Sign Up
+            <button
+              className="btn btn-block btn-sm mt-2 border border-slate-700"
+              disabled={loading}
+            >
+              {loading ? 'Loading...' : 'Sign Up'}
             </button>
           </div>
         </form>
