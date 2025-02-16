905471bd0faa0fccdd930696ca889bed157a5dcc
Megagig <megagigsoftwaresolutions.com>
2025-02-16 17:46:14 +0100

    Install Daisy UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline text-red-500"> welcome</h1>
+      <button className="btn btn-primary">click here</button>
     </>
   );
 }
