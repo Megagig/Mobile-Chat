61cccfeb0bfd82c6ae91d9824aaccfb4d1abc00b
Megagig <megagigsoftwaresolutions.com>
2025-02-17 19:37:20 +0100

    Setup socketio in the backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,19 +2,19 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
+import { app, server } from './socket/socket.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-const app = express();
 app.use(cookieParser()); // for parsing cookies
 app.use(express.json()); // for parsing application/json
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/messages', messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
